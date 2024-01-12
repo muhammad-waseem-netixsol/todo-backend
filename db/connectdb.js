af58dc429cdd0a85034df29e0f10fa8116fabc16
muhammad-waseem-netixsol <muhammad_waseem_intern@netixsol.com>
2024-01-12 11:35:05 +0500

    final

--- db/connectdb.js
+++ db/connectdb.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require('dotenv').config()
 const connectDB = async () => {
   try {
-    await mongoose.connect(mongodb://gorayausman061:Z0GiGTFr4JIPiUvl@ac-3i0kfuy-shard-00-00.vdk3ku9.mongodb.net:27017,ac-3i0kfuy-shard-00-01.vdk3ku9.mongodb.net:27017,ac-3i0kfuy-shard-00-02.vdk3ku9.mongodb.net:27017/CMS?ssl=true&replicaSet=atlas-qooh9o-shard-0&authSource=admin&retryWrites=true&w=majority");
+    await mongoose.connect("mongodb://gorayausman061:Z0GiGTFr4JIPiUvl@ac-3i0kfuy-shard-00-00.vdk3ku9.mongodb.net:27017,ac-3i0kfuy-shard-00-01.vdk3ku9.mongodb.net:27017,ac-3i0kfuy-shard-00-02.vdk3ku9.mongodb.net:27017/CMS?ssl=true&replicaSet=atlas-qooh9o-shard-0&authSource=admin&retryWrites=true&w=majority");
     console.log("Database connected successfully...");
   } catch (error) {
     throw new Error("Error connecting to the database:", error);
